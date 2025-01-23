967d055bb6bb9353fec971655e4beb4ecc49bce4
Maria Oros <maria.oros1201@gmail.com>
2025-01-22 21:17:37 -0600

    Update exp.R

--- exp.R
+++ exp.R
@@ -1,2 +1,3 @@
 
+rmarkdown::render("exponential-distribution-simulation/simulation_report.Rmd", output_format = "pdf_document")
 rmarkdown::render("exploring-ToothGrowth-dataset/eda.Rmd", output_format = "pdf_document")
